23d2ee25f3d037eef8861fbbdbf77df41cabf230
lin MA <lin.ma@ieseg.fr>
2023-11-02 06:11:13 +0100

    手动定义的方式转变为使用 resources 方法

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get "restaurants", to: "restaurants#index"
-  get "restaurants/new", to: "restaurants#new"
-  post "restaurants", to: "restaurants#create"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "restaurants/:id/edit", to: "restaurants#edit"
-  patch "restaurants/:id", to: "restaurants#update"
-  delete "restaurants/:id", to: "restaurants#destroy"
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit"
+  # patch "restaurants/:id", to: "restaurants#update"
+  # delete "restaurants/:id", to: "restaurants#destroy"
   # 路由匹配的顺序很重要。将动态路由规则放在静态路由规则之后。这样，静态路由将首先被匹配，而不会与动态路由发生冲突。
+  resources :restaurants
 end
